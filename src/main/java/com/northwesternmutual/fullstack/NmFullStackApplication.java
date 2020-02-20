82e3e0eccfff2b5bf2d14a4c2c0cbc0d4ab4a5bf
Mukarram Mukhtar <mukarram@hotmail.com>
2020-02-19 20:34:34 -0600

    First commit, empty project

---
+++ src/main/java/com/northwesternmutual/fullstack/NmFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.northwesternmutual.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmFullStackApplication.class, args);
+	}
+
+}
